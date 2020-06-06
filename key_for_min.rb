4eef81245f8252d5ce5171713bddae4efeeea636
edmundo papi <edmundopierre@yahoo.com>
2020-06-06 10:43:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_value = nil
-  name_hash.each do |name, value|
+  name_hash.collect do |name, value|
 
     if lowest_value == nil|| value < lowest_value
       lowest_value = value
